e459b488c806ff8c71f4e34638097e042f952bb5
ali-emumba <ali.usman@emumba.com>
2024-07-29 18:25:37 +0500

    added utils functions for synchandling, streamlining apiresponsonses and apierros, also added cors and cookieparser

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,16 @@ dotenv.config({
 
 
 import connectDB from './db/index.js'
+import { app } from "./app.js";
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.error("MONGODB CONNECTION ERROR --  connection failed ---- ", error);
+    process.exit(1);
+});
