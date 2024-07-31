2c30228c188cb01a812a898adb63a7312ebf2b49
ali-emumba <ali.usman@emumba.com>
2024-07-31 10:23:30 +0500

    added user register controller logic to create a user in mogodb and img on cloudinary

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import { asyncHandler } from "./../utils/asyncHandler.js";
 import { User } from "./../models/User.js";
+import { ApiError } from "./../utils/ApiError.js";
+import { uploadOnCLoudinary } from "./../utils/cloudinary.js";
+import { ApiResponse } from "./../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // code to register a user
@@ -28,9 +31,40 @@ User.findOne({
   email: validatedData.data.email,
 }).then((user) => {
   if (user) {
-    res.status(400);
-    throw new Error("User with this email already exists");
+    throw new ApiError(409, "User already exists");
   }
 });
+
+const avatarLocalPath = req.files?.avatar[0]?.path;
+if (!avatarLocalPath) {
+  throw new ApiError(400, "Avatar image is required");
+}
+
+const avatar = await uploadOnCLoudinary(avatarLocalPath);
+
+if (!avatar) {
+  throw new ApiError(500, "Avatar image upload failed");
+}
+
+const user = await User.create({
+  email: validatedData.data.email,
+  firstname: validatedData.data.firstname,
+  lastname: validatedData.data.lastname,
+  password: validatedData.data.password,
+  budget: validatedData.data.budget,
+  avatar: avatar.secure_url,
+});
+
+const createdUser = await User.findById(user._id).select(
+  "-password -refreshToken"
+);
+
+if (!createdUser) {
+  throw new ApiError(500, "User creation failed");
+}
+
+return res
+  .status(201)
+  .json(new ApiResponse(201, createdUser, "User Registered Successfully"));
 
 export { registerUser };
